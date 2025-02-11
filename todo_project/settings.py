8a3c03c0c1d56fa105c19cecbdedecd082409699
gitcommander78 <rawappeal@hotmail.com>
2025-02-11 15:03:44 +0100

    Fixed ALLOWED_HOSTS for Render

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -30,9 +30,6 @@ else:
 # Static files setup
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Application definition
 
 INSTALLED_APPS = [
